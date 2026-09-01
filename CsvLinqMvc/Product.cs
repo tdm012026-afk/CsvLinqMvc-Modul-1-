36ef7af100cbc4fb4ec182efc4bcf064f33dc1ad
tdm012026-afk <tdm012026@gmail.com>
2026-09-01 11:14:24 +0200

    Add Product model

---
+++ CsvLinqMvc/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsvLinqMvc
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- CsvLinqMvc/Program.cs
+++ CsvLinqMvc/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CsvLinqMvc;
+class Program
+{
+    static void Main()
+    {
+        var product = new Product();
+
+        product.Id = 1;
+        product.Name = "Laptop";
+        product.Price = 12000;
+
+        var product2 = new Product();
+
+        product2.Id = 2;
+        product2.Name = "Keyboard";
+        product2.Price = 500;
+    }
+}
